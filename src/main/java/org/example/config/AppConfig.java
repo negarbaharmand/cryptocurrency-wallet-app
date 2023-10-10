0d8f42f14b5ff12f1a0fec0c1d16bb7e6ff5de7b
Negar Baharmand <negarbaharmand@outlook.com>
2023-10-10 18:53:04 +0200

    Define beans in app config

--- src/main/java/org/example/config/AppConfig.java
+++ src/main/java/org/example/config/AppConfig.java
@@ -8,5 +8,24 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ComponentScan(basePackages = "org.example")
 public class AppConfig {
+   /* Way2:
+    @Bean
+    public TransactionDaoImpl transactionDao() {
+        return new TransactionDaoImpl();
+    }
 
+    @Bean
+    public WalletDaoImpl walletDao() {
+        return new WalletDaoImpl();
+    }
+
+    @Bean
+    public TransactionManagementImpl transactionManagement() {
+        return new TransactionManagementImpl(walletDao(), transactionDao());
+    }
+
+    @Bean
+    public WalletManagementImpl walletManagement() {
+        return new WalletManagementImpl(walletDao());
+    } */
 }
