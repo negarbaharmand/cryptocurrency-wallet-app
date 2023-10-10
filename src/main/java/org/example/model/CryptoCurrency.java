fa72f8ed92c1f3eb3bea18f10af3f3bd9e2fca7c
Negar Baharmand <negarbaharmand@outlook.com>
2023-10-10 15:46:45 +0200

    Implement Wallet model and add Spring dependencies

--- src/main/java/org/example/model/CryptoCurrency.java
+++ src/main/java/org/example/model/CryptoCurrency.java
@@ -1,2 +1,16 @@
-package org.example.model;public enum CryptoCurrency {
+package org.example.model;
+
+public enum CryptoCurrency {
+
+    BTC("Bitcoin"), ETH("Ethereum"), USDT("TetherUS"), BNB("BNB");
+
+    private final String name;
+
+    CryptoCurrency(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
--- src/main/java/org/example/model/wallet.java
+++ src/main/java/org/example/model/wallet.java
@@ -1,2 +1,71 @@
-package org.example.model;public class wallet {
+package org.example.model;
+
+import org.example.exception.InsufficientBalanceException;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class wallet {
+
+    //Fields:
+    private final String id;
+    private final String walletName;
+    private final Map<CryptoCurrency, BigDecimal> cryptoCurrencies;
+
+    public wallet(String walletName) {
+        this.id = UUID.randomUUID().toString();
+        this.walletName = walletName;
+        this.cryptoCurrencies = new HashMap<>();
+
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getWalletName() {
+        return walletName;
+    }
+
+    public Map<CryptoCurrency, BigDecimal> getCryptoCurrencies() {
+        return cryptoCurrencies;
+    }
+
+    // If the cryptocurrency doesn't have a balance we assign a zero default value
+    public BigDecimal getBalance(CryptoCurrency cryptoCurrency) {
+        return cryptoCurrencies.getOrDefault(cryptoCurrency, BigDecimal.ZERO);
+    }
+
+    public void deposit(CryptoCurrency cryptoCurrency, BigDecimal amount) {
+        if (amount.compareTo(BigDecimal.ZERO) <= 0)
+            throw new IllegalArgumentException("Deposit amount must be grater than zero.");
+
+        BigDecimal currentBalance = getBalance(cryptoCurrency);
+        BigDecimal newBalance = currentBalance.add(amount);
+
+        cryptoCurrencies.put(cryptoCurrency, newBalance);
+    }
+
+    public void withdraw(CryptoCurrency cryptoCurrency, BigDecimal amount) {
+        if (amount.compareTo(BigDecimal.ZERO) <= 0)
+            throw new IllegalArgumentException("Withdrawal amount must be grater than zero.");
+
+        BigDecimal currentBalance = getBalance(cryptoCurrency);
+        if (currentBalance.compareTo(amount) < 0)
+            throw new InsufficientBalanceException("Insufficient Balance for withdrawal. ");
+        BigDecimal newBalance = currentBalance.subtract(amount);
+
+        cryptoCurrencies.put(cryptoCurrency, newBalance);
+    }
+
+    @Override
+    public String toString() {
+        return "Wallet{" +
+                "id='" + id + '\'' +
+                ", walletName='" + walletName + '\'' +
+                ", cryptoCurrencies=" + cryptoCurrencies +
+                '}';
+    }
 }
